c6172de7ee8f7400248b57ad82675fa6cca7eca2
Meruyert Aitbayeva (Mika) <aitbayev@usc.edu>
2015-02-03 22:20:28 -0800

    assignment 1
    no style

---
+++ Song.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once __DIR__ . '/Database.php';
+
+class Song extends Database{
+    private $title;
+    private $artistID;
+    private $genreID;
+    private $price;
+    private $id;
+    
+    public function setTitle($title){
+        $this->title = $title;
+    }
+    
+    public function setArtistId($artist_id){
+        $this->artistID = $artist_id;
+    }
+    
+    public function setGenreId($genre_id){
+        $this->genreID = $genre_id;
+    }
+    
+    public function setPrice($price){
+        $this->price = $price;
+    }
+    
+    public function save(){
+        $sql = "INSERT INTO songs(title, artist_id, genre_id, price, added, created_at, updated_at)
+                 VALUES (?, ?, ?, ?, NOW(), NOW(), NOW())
+        ";
+        $statement = static::$pdo->prepare($sql);
+        
+      
+    $statement->bindParam(1, $this->title);
+    $statement->bindParam(2, $this->artistID);
+    $statement->bindParam(3, $this->genreID);
+    $statement->bindParam(4, $this->price);
+                          
+    $statement->execute();
+    
+        
+     
+        
+        $this->id = static::$pdo->lastInsertId();
+        
+    }
+    
+    public function getTitle(){
+        return $this->title;
+    }
+    
+    public function getId(){
+        return $this->id;
+    }
+}
+
+?>
